cb290e2a04f83b55474525d7240f635dda1e7cb3
Alexander Chuzhoy <achuzhoy@redhat.com>
2026-04-17 16:01:31 -0400

    osac-installer: replace fulfillment-cli with osac
    The binary name was changed.
    
    Signed-off-by: Alexander Chuzhoy <achuzhoy@redhat.com>

--- scripts/prepare-fulfillment-service.sh
+++ scripts/prepare-fulfillment-service.sh
@@ -19,12 +19,12 @@ VIRT_SERVICE=${VIRT_SERVICE:-${EXTRA_SERVICES}}
 
 # Login to fulfillment API and create hub
 FULFILLMENT_API_URL=https://$(oc get route -n ${INSTALLER_NAMESPACE} fulfillment-api -o jsonpath='{.status.ingress[0].host}')
-fulfillment-cli login --insecure --private --token-script "oc create token -n ${INSTALLER_NAMESPACE} admin" --address ${FULFILLMENT_API_URL}
-fulfillment-cli create hub --kubeconfig=kubeconfig.hub-access --id hub --namespace ${INSTALLER_NAMESPACE}
+osac login --insecure --private --token-script "oc create token -n ${INSTALLER_NAMESPACE} admin" --address ${FULFILLMENT_API_URL}
+osac create hub --kubeconfig=kubeconfig.hub-access --id hub --namespace ${INSTALLER_NAMESPACE}
 
 # Wait for computeinstancetemplate to exist (VMaaS only)
 if [[ "${VIRT_SERVICE}" == "true" ]]; then
-    retry_until 1200 5 '[[ -n "$(fulfillment-cli get computeinstancetemplate -o json | jq -r --arg tpl ${INSTALLER_VM_TEMPLATE} '"'"'select(.id == $tpl)'"'"' 2> /dev/null)" ]]' || {
+    retry_until 1200 5 '[[ -n "$(osac get computeinstancetemplate -o json | jq -r --arg tpl ${INSTALLER_VM_TEMPLATE} '"'"'select(.id == $tpl)'"'"' 2> /dev/null)" ]]' || {
         echo "Timed out waiting for computeinstancetemplate to exist"
         exit 1
     }
